d6d3c22d794898142ef67c6b84cbc05128e1ee5c
Jinho <dandyday31@gmail.com>
2021-10-01 14:48:53 +0000

    BOJ 10845

---
+++ BOJ/10845.py
@@ -0,0 +1,30 @@
+import sys
+
+
+n = int(sys.stdin.readline())
+stack = []
+for i in range(n):
+    ipt = sys.stdin.readline()
+    try:
+        command, num = ipt.split()
+        num = int(num)
+    except ValueError:
+        command = ipt.split()[0]
+    if command == "push":
+        stack.append(num)
+    elif command == "pop":
+        if len(stack) != 0:
+            print(stack.pop(0))
+        else:
+            print(-1)
+    elif command == "size":
+        print(len(stack))
+    elif command == "empty":
+        if len(stack) == 0: print(1)
+        else: print(0)
+    elif command == "back":
+        if len(stack) == 0: print(-1)
+        else: print(stack[-1])
+    elif command == "front":
+        if len(stack) == 0: print(-1)
+        else: print(stack[0])
