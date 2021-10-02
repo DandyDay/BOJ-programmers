1e48ed80237d7279e480bcb32816a4d3a0be9dd0
Jinho <dandyday31@gmail.com>
2021-10-02 14:46:00 +0000

    BOJ 1158

---
+++ BOJ/1158.py
@@ -0,0 +1,12 @@
+import sys
+n, m = map(int, sys.stdin.readline().split())
+idx = 0
+lst = list(range(1, n+1))
+result = []
+while(lst):
+    idx = (idx + m - 1) % (len(lst))
+    result.append(str(lst.pop(idx)))
+
+print("<", end="")
+print(", ".join(result), end="")
+print(">")
