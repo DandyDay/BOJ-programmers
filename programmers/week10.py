d7db2aaf970717af75f8e9ea5e6abb21b0218ece
Jinho <dandyday31@gmail.com>
2021-10-22 13:49:44 +0000

    programmers week 10

---
+++ programmers/week10.py
@@ -0,0 +1,22 @@
+line = [[2, -1, 4], [-2, -1, 4], [0, -1, 1], [5, -8, -12], [5, 8, 12]]	
+xans = []
+yans = []
+
+for i, l in enumerate(line):
+    for j, m in enumerate(line):
+        if i < j and l[0]*m[1] - m[0]*l[1] != 0:
+            x = (l[1]*m[2] - l[2]*m[1]) / (l[0]*m[1] - m[0]*l[1])
+            y = (l[2]*m[0] - l[0]*m[2]) / (l[0]*m[1] - m[0]*l[1])
+            if(x == int(x) and y == int(y)):
+                xans.append(int(x))
+                yans.append(int(y))
+answer = []
+for i in range(max(yans) - min(yans) + 1):
+    answer.append("." * (max(xans) - min(xans) + 1))
+
+for i in range(len(xans)):
+    answer[yans[i]-min(yans)] = answer[yans[i]-min(yans)][:xans[i]-min(xans)] + "*" + answer[yans[i]-min(yans)][xans[i]-min(xans)+1:]
+
+answer.reverse()
+
+print(answer)
